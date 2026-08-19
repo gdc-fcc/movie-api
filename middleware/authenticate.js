876772f8ea24bc08f9b28db2d59e0540a9e47d00
Gregor de Cillia <de.cillia.gregor@gmail.com>
2026-08-19 15:50:10 +0200

    add authentication middleware (untested)

---
+++ middleware/authenticate.js
@@ -0,0 +1,18 @@
+import {verifyToken} from "../utils/jwt.js"
+
+const authenticate = (req, res, next) => {
+    const header = req.header.bearer
+    const authHeader = req.headers.authorization;
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+        return res.status(401).json({ message: "No token provided" });
+    }
+    const token = authHeader.split(" ")[1];
+    const payload = verifyToken(token)
+    if (payload === null) {
+        return res.status(401).json({"error": "Invalid or expired token."})
+    }
+    req.user = payload
+    next()
+}
+
+export {authenticate}
