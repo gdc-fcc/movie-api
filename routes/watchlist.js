1c7e70f9c04d80a8c8111d5df8de37d44312ea31
Gregor de Cillia <de.cillia.gregor@gmail.com>
2026-08-19 18:46:41 +0200

    add route level authentication to /api/watchlist

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -1,8 +1,10 @@
 import express from "express"
 import * as db from "../utils/db.js"
+import {authenticate} from "../middleware/authenticate.js"
 
 const route = express.Router()
 
+route.use(authenticate)
 route.get("/:userId/movies", (req, res) => {
     const userId = req.params.userId
     return res.status(200).json(db.getWatchlist(parseInt(userId)))
