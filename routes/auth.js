3b12046b5e60f95f368a1d0c03b98debc9c7c9f3
Gregor de Cillia <de.cillia.gregor@gmail.com>
2026-08-19 21:08:36 +0200

    only compare hashes in /login

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 import express from "express"
 import {findByUsername} from "../utils/db.js"
 import {signToken} from "../utils/jwt.js"
+import {compareSync} from "bcryptjs"
 
 const route = express.Router()
 
@@ -13,8 +14,7 @@ route.post("/login", (req, res) => {
         return res.status(400).json({error: "username or password missing"})
     }
     const user = findByUsername(username)
-    // TODO: check against hashed password. Needs rehashing
-    if (user === [] || user._password != password) {
+    if (user === [] || !compareSync(password, user.passwordHash)) {
         return res.status(401).json({error: "invalid credentials"})
     }
     const token = signToken({id: user.id, rule: user.role})
