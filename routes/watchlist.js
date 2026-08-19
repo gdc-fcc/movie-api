4b3c42bd506eff84372ff8fc956050f089139143
Gregor de Cillia <de.cillia.gregor@gmail.com>
2026-08-19 15:48:39 +0200

    import full db module in watchlist router

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -1,11 +1,11 @@
 import express from "express"
-import {getWatchlist} from "../utils/db.js"
+import * as db from "../utils/db.js"
 
 const route = express.Router()
 
 route.get("/:userId/movies", (req, res) => {
     const userId = req.params.userId
-    return res.status(200).json(getWatchlist(parseInt(userId)))
+    return res.status(200).json(db.getWatchlist(parseInt(userId)))
 })
 
 export default route
